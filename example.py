44e50007305a549ae46ee29195f07acffe28712b
reliefs <6hourapp@gmail.com>
2019-12-23 04:20:05 +0100

    added bot file and class

--- example.py
+++ example.py
@@ -1,19 +1,7 @@
-from tiktokapi import api
+from tiktokapi import bot
 
-api = api.Api()
+tiktok = bot.Bot()
+tiktok.download_user_videos("maskofshiva")
 
-videos = api.get_user_videos("maskofshiva")
 
-x = 1
-for video in videos:
-    print(video)
-    video_urls = api.get_video_url(video)
-    print(video_urls)
-    try:
-        api.download_user_video(video_urls, str(x))
-    except Exception as e:
-        print(e)
-    print(api.get_meta_title(video))
-    print(api.get_likes_count(video), "likes")
-    print(api.get_comment_count(video), "comments")
-    x += 1
+
